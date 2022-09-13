115689d00e549eeb43c97b3c5098d713247cde78
Chet Hay <chet.hay@gmail.com>
2022-09-13 11:46:57 -0400

    installed nvim-ufo for folding.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -118,6 +118,12 @@ return packer.startup(function()
 	--  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 	--  CONTROLS              
 	--  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+	use({ "kevinhwang91/nvim-ufo", requires = "kevinhwang91/promise-async" })
+	require("ufo").setup({
+		provider_selector = function(bufnr, filetype, buftype)
+			return { "treesitter", "indent" }
+		end,
+	})
 	use({
 		"akinsho/nvim-toggleterm.lua",
 		tag = "v2.*",
